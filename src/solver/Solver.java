b0c8f8e5849ddd7a38cdebbc18ef122eadf08264
repozytorium1 <26385959+repozytorium1@users.noreply.github.com>
2018-12-28 23:34:40 +0100

    update from master

--- src/solver/Solver.java
+++ src/solver/Solver.java
@@ -1,75 +1,64 @@
 package solver;
 
 
-import data_io.DataInput;
-import data_structure.board;
-import data_structure.gameState;
+import data_structure.Board;
+import data_structure.GameState;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Vector;
 
 public class Solver {
-    
 
-    Comparator<gameState> comparator = new Comparator<gameState>() {
+    Comparator<GameState> comparator = new Comparator<GameState>() {
         @Override
-        public int compare(gameState o1, gameState o2) {
+        public int compare(GameState o1, GameState o2) {
             return o1.getHeuristicValue() - o2.getHeuristicValue();
         }
     };
 
-    private PriorityQueue<gameState> queue = new PriorityQueue<>(comparator);
-    private board template;
+    private PriorityQueue<GameState> queue = new PriorityQueue<>(comparator);
+    private Board template;
 
-    public Solver(board board, int initialValue) {
-//        template = board;board.setField(4,1,9);
-//        System.out.println("Solver() board\n" + board.toString());
-        template = new board(board);
-//        System.out.println("Solver() template\n" + template.toString());
-//        template.setField(4,2,7);
-//        template.setField(1,4,2);
-//        template.setField(1,3,9);
-//        template.setField(2,4,1);
-//        template.setField(3,2,9);
-//        template.setField(2,2,8);
-//        template.setField(2,1,6);
-//        template.setField(3,1,8);
-//        template.setField(2,3,2);
-//        template.setField(3,3,7);
-//        System.out.println("Solver() after setfield template\n" + template.toString());
-//        System.out.println("Solver() after setfield board\n" + board.toString());
-        gameState initial = new gameState(template.generateShortcut(), initialValue, false);
+    public Solver(Board board, int initialValue) {
+        System.out.println("Solver() board\n" + board.toString());
+        template = new Board(board);
+        System.out.println("Solver() template\n" + template.toString());
+        System.out.println("Solver() after setfield template\n" + template.toString());
+        System.out.println("Solver() after setfield board\n" + board.toString());
+        GameState initial = new GameState(template.generateShortcut(), initialValue, false);
         System.out.println(template.generateShortcut());
         queue.add(initial);
     }
 
 
-    public board solve() {
-//        DataInput di = new DataInput();
-//        di.ReadBoard("examples/board_6x6.txt");
-//        di.ReadBoard("examples/example.txt");
+    public Board solve() {
+        GameState analyzed;
+        Board analyzedBoard = null;
+        int countOfAnalyzed = 0;
 
-        gameState analyzed = null;
-        board analyzedBoard = null;
-
-        tu:
         while (queue.isEmpty() == false) {
+            ++countOfAnalyzed;
             analyzed = queue.remove();
-            analyzedBoard = new board(template).generate(analyzed.getBoardShortcut());
+            analyzedBoard = new Board(template, analyzed.getBoardShortcut());
+
+            if (!analyzedBoard.isPossibleToSolve()) {
+                System.out.println("FALSE BOARD " + countOfAnalyzed);
+                continue;
+            }
             if (analyzed.isSolved()) {
                 System.out.println("" + analyzed.isSolved() + " " + analyzed.getBoardShortcut());
                 System.out.println(analyzedBoard);
-                break tu;
+                break;
             }
 
-            Vector<gameState> newGeneratedStates = analyzedBoard.nextStep();
+            Vector<GameState> newGeneratedStates = analyzedBoard.nextStep();
             for (int i = 0; i < newGeneratedStates.size(); ++i) {
                 queue.add(newGeneratedStates.elementAt(i));
                 System.out.println("" + analyzed.isSolved() + " " + newGeneratedStates.elementAt(i).getBoardShortcut());
             }
-
         }
+        System.out.println("Analyzed board: " + countOfAnalyzed);
 
         return analyzedBoard;
     }
