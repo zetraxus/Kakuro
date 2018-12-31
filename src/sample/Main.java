a12a2ebf35477e99127d695707d8b0d8ec2b9222
repozytorium1 <26385959+repozytorium1@users.noreply.github.com>
2018-12-31 05:56:42 +0100

    calculating heuristic values

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -17,9 +17,9 @@ import java.io.PrintWriter;
 public class Main extends Application {
 
     public static void main(String[] args) {
-        BoardGenerator i = new BoardGenerator(30);
-
-        String inputFile = "examples/example.in";
+        BoardGenerator i = new BoardGenerator(8);
+        long start = System.nanoTime();
+        String inputFile = "examples/interesting.in";
         String outputFile = null;
 
         if (args.length >= 1)
@@ -28,13 +28,15 @@ public class Main extends Application {
             outputFile = args[1];
 
         DataInput di = new DataInput();
-//        di.readString(i.toString());
-        di.ReadBoard(inputFile);
+        di.readString(i.toString());
+//        di.ReadBoard(inputFile);
 
         Board template = di.makeGameBoard();
         System.out.println("main:\n" + template.toString());
-        Solver solver = new Solver(template, template.getCost()); // TODO compute initial value
+        Solver solver = new Solver(template, 0); // TODO compute initial value
         Board result = solver.solve();
+            long end=System.nanoTime();
+            System.out.println("time: "+(double)(end-start)/1000000000);
 
         if (outputFile != null)
             writeResultToFile(result, outputFile);
